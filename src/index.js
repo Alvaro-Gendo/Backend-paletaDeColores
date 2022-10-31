bba4f2b9c62f5d61b18a0d55cbc0af69ca0b77d2
Alvaro Ruiz Mejia <zhuone@hotmail.com>
2022-10-31 20:56:26 -0300

    agreuge ruta y middlewares

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import path from "path";
 
 const app = express();
 
@@ -7,3 +10,10 @@ app.listen(app.get("port"), () =>{
     console.log("Puerto " + app.get("port"))
 })
 
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname, "../public")))
+
+app.use("/apicolor" , rutas)
---
+++ src/routes/color.routes.js
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const rutas = Router();
+
+rutas.route("/color")
